179e3fd0f90d2d8b9257a3f895d536d1b5b85229
avadh-191 <avadhvekariya5404@gmail.com>
2026-06-01 15:43:55 +0530

    try

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ function App() {
                 >
                   <use href="/icons.svg#github-icon"></use>
                 </svg>
-                GitHub djkfndsjfnkjfn
+                GitHub
               </a>
             </li>
             <li>

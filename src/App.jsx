c9c66755393fd9e4fce1e0c9950186bbbc3c4044
avadh-191 <avadhvekariya5404@gmail.com>
2026-06-02 16:20:35 +0530

    wheather app creating

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+
+import{ useEffect } from "react";
+
+import{FaWind} from "react-icons/fa";
+import{FaDroplet} from "react-icons/fa6";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [city, setCity] = useState("London");
+  const [weather, setWeather] = useState(null);
+  const [lastCity, setLastCity] = useState("");
+
+  const getWeather = async () => {
+   if (!city || city === lastCity) return;
+
+  setLastCity(city);
+
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=77f3cb2fcfee45399c780454260206&q=${city}&aqi=yes`
+    );
+
+    const data = await response.json();
+
+    if (!data.error) {
+      setWeather(data);
+    } else {
+      alert("City not found");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="weather-container">
+      <div className="weather-card">
+        <h1>Weather App</h1>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Enter City"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button onClick={getWeather}>Search</button>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+{weather && (
+  <div className="weather-info">
+    <h2>{weather.location.name}</h2>
+
+    <img
+      src={weather.current.condition.icon}
+      alt={weather.current.condition.text}
+    />
+
+    <h3>{weather.current.temp_c}°C</h3>
+    <p>{weather.current.condition.text}</p>
+
+    <div className="details">
+      <div>
+        <strong>Humidity</strong>
+        <p ><FaDroplet size={20} /> {weather.current.humidity}%</p>
+      </div>
+
+      <div>
+        <strong>Wind</strong>
+        <p><FaWind size={20}/> {weather.current.wind_kph} km/h</p>
+      </div>
+    </div>
+  </div>
+)}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
